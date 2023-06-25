d51631eeba2ebdbb2f1d30f6c20202db5cc3892d
Kenzo <suprabluesil@gmail.com>
2023-06-25 11:43:39 +0800

    added Todo request, routes and views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/todos/index', [TodoController::class, 'index'])->name('todos.index');
+Route::get('/todos/create', [TodoController::class, 'create'])->name('todos.create');
+Route::post('/todos/store', [TodoController::class, 'store'])->name('todos.store');
+Route::get('/todos/show/{id}', [TodoController::class, 'show'])->name('todos.show');
+Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+Route::put('/todos/update', [TodoController::class, 'update'])->name('todos.update');
+Route::delete('/todos/destroy', [TodoController::class, 'destroy'])->name('todos.destroy');
